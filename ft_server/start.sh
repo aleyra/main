0c265acecd460d36b4569fee4dbeb94899c89716
lburnet <75073111+aleyra@users.noreply.github.com>
2021-02-18 13:52:38 +0100

    Update start.sh

--- ft_server/start.sh
+++ ft_server/start.sh
@@ -1,6 +1,6 @@
-docker build . -t ft_server
+odocker build . -t ft_server
 docker run -it -p 80:80 -p 443:443 --name test --rm ft_server
 open http://0.0.0.0
-open https://0.0.0.0
-open http://0.0.0.0/wordpress
-open http://0.0.0.0/phpmyadmin
+echo "ouvrir https://0.0.0.0 puis http://0.0.0.0/wordpress et http://0.0.0.0/phpmyadmin"
+echo "tester wordpress jusqu'a l'affichage de nouveaux elements et verifier leurs existance dans phpmyadmin"
+echo "tester l'affichage de l'autoindex avec 0.0.0.0/phpmyadmin/templates par exemple"
